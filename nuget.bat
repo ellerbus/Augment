40c690544f80cbf83b79b26f0c84880e44c92c54
Stu Ellerbusch <stuart.ellerbusch@lmco.com>
2016-12-02 10:22:04 -0500

    Flatten Folder Hiearchy

---
+++ nuget.bat
@@ -0,0 +1,33 @@
+cls
+@echo off
+color 0A
+
+setlocal
+
+	set sln_path=%~dp0
+
+	set proj_nm=%~1
+
+	set cs_proj=%sln_path%\%proj_nm%\%proj_nm%.csproj
+	
+	echo ... Project: %cs_proj%
+	echo .
+
+	set pkg_path=%nu_path%NuGetPackages
+
+	if not exist %pkg_path% (
+		echo Making %pkg_path%
+		mkdir %pkg_path%
+	)
+
+	if exist %pkg_path%\%proj_nm%.*.nupkg (
+		echo ... Deleting %proj_nm%.*.nupkg
+		echo .
+		del /F /Q %pkg_path%\%proj_nm%.*.nupkg
+	)
+
+	echo ... packing
+	echo .
+	%sln_path%\nuget.exe pack "%cs_proj%" -OutputDirectory "%pkg_path%" -Properties Configuration=Release
+	
+endlocal
